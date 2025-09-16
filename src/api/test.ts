7b143d1b15105e482a4e8b31cc742900b544c706
ikki-wiki <2013620@student.uma.pt>
2025-09-16 19:33:54 +0100

    test 4

--- src/api/test.ts
+++ src/api/test.ts
@@ -1,5 +1,13 @@
+const LOCAL_API = "http://localhost:5000";
+const PROD_API = "https://task-manager-backend-9yge.onrender.com";
+
+const API_URL =
+  import.meta.env.PROD // Vite sets this to true in production build
+    ? PROD_API
+    : LOCAL_API;
+
 export async function getHello() {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/`);
+  const res = await fetch(`${API_URL}/`);
   if (!res.ok) throw new Error("Failed to fetch");
   return res.text();
 }
