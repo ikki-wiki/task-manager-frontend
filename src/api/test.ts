d6d707c2845af1aea3e7ff028188f4bf6aa5c77a
ikki-wiki <2013620@student.uma.pt>
2025-09-16 19:38:03 +0100

    fix test 5

--- src/api/test.ts
+++ src/api/test.ts
@@ -1,11 +1,11 @@
-const LOCAL_API = "http://localhost:5000";
+/*const LOCAL_API = "http://localhost:5000";
 const PROD_API = "https://task-manager-backend-9yge.onrender.com";
 
 const API_URL =
   import.meta.env.PROD // Vite sets this to true in production build
     ? PROD_API
     : LOCAL_API;
-
+*/
 export async function getHello() {
   const res = await fetch(`https://task-manager-backend-9yge.onrender.com/`);
   if (!res.ok) throw new Error("Failed to fetch");
